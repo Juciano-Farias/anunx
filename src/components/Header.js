7ae945bb6093f398e6e7ab1de67e16906ce80783
Juciano <juciano.junior@hotmail.com>
2021-12-07 17:52:27 +0000

    Remodelando a Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,59 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from 'react'
+import Link from 'next/link'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import { Avatar, Container, Menu } from '@material-ui/core'
+import { AccountCircle } from '@material-ui/icons'
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
   },
-}));
+  userName: {
+    marginLeft: '6px'
+  }
+}))
 
 export default function ButtonAppBar() {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
+    <>
+      <AppBar position="static" elevation={3}>
+        <Container maxWidth="lg">
+          <Toolbar>
+            <Link href="/" passHref>
+            <Typography variant="h6" className={classes.title}>
+              Anunx
+            </Typography>
+            </Link>
+            <Link href="/user/publish" passHref>
+              <Button color="inherit" variant="outlined">
+                Anunciar e Vender
+              </Button>
+            </Link>
+            <IconButton color="secondary">
+              {
+                true === false
+                 ? <Avatar src="" />
+                 : <AccountCircle />
+              }
+              <Typography variant="subtitle2" color="secondary" className={classes.userName}>
+                Juciano Gomes
+              </Typography>
+            </IconButton>
+            
+          </Toolbar>
+        </Container>
       </AppBar>
-    </div>
-  );
+    </>
+  )
 }
