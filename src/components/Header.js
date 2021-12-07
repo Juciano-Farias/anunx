f6450ea9180e09a882b2a258814452c8435ff796
Juciano <juciano.junior@hotmail.com>
2021-12-07 18:08:36 +0000

    Header - adicionando menu para o usuario

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
@@ -7,7 +8,7 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
-import { Avatar, Container, Menu } from '@material-ui/core'
+import { Avatar, Container, Divider, Menu, MenuItem } from '@material-ui/core'
 import { AccountCircle } from '@material-ui/icons'
 
 const useStyles = makeStyles((theme) => ({
@@ -19,11 +20,17 @@ const useStyles = makeStyles((theme) => ({
   },
   userName: {
     marginLeft: '6px'
+  },
+  divider: {
+    margin: '8px 0'
   }
 }))
 
 export default function ButtonAppBar() {
   const classes = useStyles()
+  const [anchorUserMenu, setAnchorUserMenu] = useState(false)
+
+  const openUserMenu = Boolean(anchorUserMenu)
 
   return (
     <>
@@ -40,7 +47,7 @@ export default function ButtonAppBar() {
                 Anunciar e Vender
               </Button>
             </Link>
-            <IconButton color="secondary">
+            <IconButton color="secondary" onClick={(e) => setAnchorUserMenu(e.currentTarget)}>
               {
                 true === false
                  ? <Avatar src="" />
@@ -50,7 +57,24 @@ export default function ButtonAppBar() {
                 Juciano Gomes
               </Typography>
             </IconButton>
-            
+
+            <Menu
+              anchorEl={anchorUserMenu}
+              open={openUserMenu}
+              onClose={() => setAnchorUserMenu(null)}
+              getContentAnchorEl={null}
+              anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+              transformOrigin={{vertical: 'top', horizontal: 'center'}}
+            >
+              <Link href="/user/dashboard" passHref>
+                <MenuItem>Meus anúncios</MenuItem>
+              </Link>
+              <Link href="/user/publish" passHref>
+                <MenuItem>Publicar novo anúncio</MenuItem>
+              </Link>
+              <Divider className={classes.divider} />
+              <MenuItem>Sair</MenuItem>
+            </Menu>
           </Toolbar>
         </Container>
       </AppBar>
