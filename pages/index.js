d03c1ff432b3424b1abe745c11f4da59d78ac1a5
Juciano <juciano.junior@hotmail.com>
2021-12-07 17:23:27 +0000

    adicionando home deslogada

---
+++ pages/index.js
@@ -0,0 +1,118 @@
+import {
+    Container,
+    IconButton,
+    InputBase,
+    Paper,
+    Typography,
+    Grid,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Button,
+} from '@material-ui/core'
+
+import SearchIcon from '@material-ui/icons/search'
+import TemplateDefault from '../src/templates/Default'
+import { makeStyles } from '@material-ui/core'
+import { ThemeContext } from '@emotion/react'
+
+const useStyels = makeStyles((theme) => ({
+    searchContainer: {
+        padding: theme.spacing(8, 10, 6)
+    },
+    searchBox: {
+        display: 'flex',
+        justifyContent: 'center',
+        padding: theme.spacing(0, 2),
+        marginTop: '20px',
+    },
+    cardMedia: {
+        paddingTop: '56%'
+      }, 
+}))
+
+const Home = () => {
+    const classes = useStyels()
+    return(
+        <TemplateDefault>
+            <Container maxWidth="md" className={classes.searchContainer}>
+                <Typography component="h1" variant="h3" align="center" color="textPrimary">
+                    O que deseja encontrar?
+                </Typography>
+                <Paper className={classes.searchBox}>
+                    <InputBase 
+                        placeholder="Ex:.. iPhone 12 mini com garantia"
+                        fullWidth
+                    />
+                    <IconButton>
+                        <SearchIcon />
+                    </IconButton>
+                </Paper>
+            </Container>
+
+            
+            <Typography component="h2" variant="h4" align="center" color="textPrimary">
+                Destaques
+            </Typography>
+            <br />
+            <Container maxWidth="md" className={classes.cardGrid}>
+                <Grid container spacing={4}>
+                    <Grid item xs={12} sm={6} md={4} >
+                        <Card>
+                        <CardMedia 
+                            className={classes.cardMedia}
+                            image={'https://source.unsplash.com/random'}
+                            title="titulo da imagem"
+                        />
+                        <CardContent>
+                            <Typography component="h2" variant="h5">
+                                Produto X
+                            </Typography>
+                            <Typography>
+                                R$: 60,00
+                            </Typography>
+                        </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} >
+                        <Card>
+                        <CardMedia 
+                            className={classes.cardMedia}
+                            image={'https://source.unsplash.com/random'}
+                            title="titulo da imagem"
+                        />
+                        <CardContent>
+                            <Typography component="h2" variant="h5">
+                                Produto X
+                            </Typography>
+                            <Typography>
+                                R$: 60,00
+                            </Typography>
+                        </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} >
+                        <Card>
+                        <CardMedia 
+                            className={classes.cardMedia}
+                            image={'https://source.unsplash.com/random'}
+                            title="titulo da imagem"
+                        />
+                        <CardContent>
+                            <Typography component="h2" variant="h5">
+                                Produto X
+                            </Typography>
+                            <Typography>
+                                R$: 60,00
+                            </Typography>
+                        </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Container>
+        </TemplateDefault>
+    )
+}
+
+export default Home
