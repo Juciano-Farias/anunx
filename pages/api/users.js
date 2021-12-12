98ca1042c675766dd9435efd3242cd45a5073615
Juciano <juciano.junior@hotmail.com>
2021-12-12 22:19:32 +0000

    Revert "criando model de usuarios"
    This reverts commit 759cbae4a8644fc2ad1c3da082f614e24b03b205.

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,7 +1,5 @@
 import dbConnect from '../../src/utils/dbConnect'
-import { crypto } from '../../src/utils/password'
-import UsersModel from '../../src/models/users'
- 
+
 const users = async (req, res) => {
     const { method } = req
 
@@ -10,34 +8,6 @@ const users = async (req, res) => {
             await dbConnect()
             res.status(200).json({ success: true })
             break
-
-        case 'POST':
-            // pegar os dados que vem na request
-            //  conectar com o banco de dados
-            // criptografar as senhas
-            //  salvar os dados
-            //  responder sucesso
-
-            const {
-                name,
-                email,
-                password
-            } = req.body
-
-            await dbConnect()
-
-            const passwordCrypto = await crypto(password)
-
-            const user = new UsersModel({
-                name,
-                email,
-                password: passwordCrypto,
-            })
-
-            user.save()
-
-            res.status(200).json({ success: true })
-
     }
 }
 
