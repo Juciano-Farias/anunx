9e3613f1aa7c337fa0eb224985b0db7094eb19d8
Juciano <juciano.junior@hotmail.com>
2021-12-24 16:30:47 +0000

    Integrando a pagina de produto com a API

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,111 +1,109 @@
 // Processamento de arquivos recebidos em formulário. Devemos ter cuidado na hora de tratar esses dados antes de enviar pra API
 
+import fs from 'fs'
 import path from 'path'
 import formidable from 'formidable-serverless'
-import ProductsModels from '../models/products'
+import ProductsModel from '../models/products'
 import dbConnect from '../utils/dbConnect'
-import fs from 'fs'
 
-const post = async (req,res) => {
-    await dbConnect( )
+const post = async (req, res) => {
+await dbConnect()
 
-    const form = new formidable.IncomingForm({
-        multiples: true,
-        uploadDir: 'public/uploads',
-        keepExtensions: true,
-    })
+const form = new formidable.IncomingForm({
+    multiples: true,
+    uploadDir: 'public/uploads',
+    keepExtensions: true,
+})
 
-    form.parse(req, async (error, fields, data) => {
-        if(error) {
-            return res.status(500).json({ success: false })
-        }
+form.parse(req, async (error, fields, data) => {
+    if (error){
+        return res.status(500).json({ success: false })
+    }
 
-        const { files } = data
+    const { files } = data
 
-        const filesToRename = files instanceof Array
+    const filesToRename = files instanceof Array
         ? files
         : [files]
+    
 
-        const filesToSave = []
+    const filesToSave = []
 
-        filesToRename.forEach(file => {
-            const timeStamp = Date.now()
-            const random = Math.floor(Math.random() * 999999999) + 1
-            const extension = path.extname(file.name)
+    filesToRename.forEach((file) => {
+        const timestamp = Date.now()
+        const random = Math.floor(Math.random() * 99999999) + 1
+        const extension = path.extname(file.name)
 
-            const fileName = `${timeStamp}_${random}${extension}`
+        const filename = `${timestamp}_${random}${extension}`
 
-            const oldPath = path.join(__dirname, '../../../../' + file.path)
-            const newPath = path.join(__dirname, '../../../../' + form.uploadDir + '/' + fileName)
+        const oldpath = path.join(__dirname, `../../../../../${file.path}`)
+        const newpath = path.join(__dirname, `../../../../../${form.uploadDir}/${filename}`)
 
-            filesToSave.push({
-                name: fileName,
-                path: newPath,
-            })
-
-            fs.rename(oldPath, newPath, (err) => {
-                if (err){
-                    console.log(err)
-                    return res.status(500).json({ success: false })
-                }
-            })
-
+        filesToSave.push({
+            name: filename,
+            path: newpath,
         })
 
-        const {
-            title,
-            category,
-            description,
-            price,
+        fs.rename(oldpath, newpath, (error) => {
+            if (error) {
+                console.log(error)
+                return res.status(500).json({ success: false})
+            }
+        })
+    })
+
+    const {
+        title,
+        category,
+        description,
+        price,
+        name,
+        email,
+        phone,
+        userId,
+        image,
+    } = fields
+
+    const product = new ProductsModel({
+        title,
+        category,
+        description,
+        price,
+        user:{
+            id: userId,
             name,
             email,
             phone,
-            userId,
             image,
-        } = fields
-
-        const product = new ProductsModels({
-            title,
-            category,
-            description,
-            price,
-            user: {
-                id: userId,
-                name,
-                email,
-                phone,
-                image,
-            },
-            files: filesToSave,
-        })
-
-        const register = await product.save()
-
-        if (register) {
-            res.status(201).json({ success: true })
-        } else {
-            res.status(500).json({ success: true })
-        }
-
-        res.status(200).json({ success: true })
+        },
+        files: filesToSave,
     })
+
+    const register = await product.save()
+
+    if (register) {
+        res.status(201).json({ success: true })
+    } else {
+        res.status(500).json({ success: false })
+    }
+})
 }
 
 const remove = async (req, res) => {
-    await dbConnect()
+await dbConnect()
 
-    const id = req.body.id
+const id = req.body.id
 
-    const deleted = await ProductsModels.findOneAndRemove({ _id: id })
+const deleted = await ProductsModel.findOneAndRemove({ _id: id })
 
-    if (deleted){
-        return res.status(200).json({ success: true })
-    } else {
-        return res.status(500).json({ success: false })
-    }
+if (deleted){
+    return res.status(200).json({ success: true })
+} else {
+    return res.status(500).json({ success: false })
+}
 }
 
 export {
-    post,
-    remove,
+post,
+remove,
 }
